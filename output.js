618d7a86208705ec69adab6c832b2eb4bee6abda
Oliver Caine <olliecaine@gmail.com>
2017-10-25 20:05:43 +1100

    Output code

---
+++ output.js
@@ -0,0 +1,157 @@
+// 'desc'
+describe('description', function() {
+    // body...
+});
+
+// 'ita'
+it('description', function(done) {
+    // body...
+});
+
+// 'itp'
+it('description');
+
+// 'its'
+it('description', function() {
+    // body...
+});
+
+// 'cl'
+console.log(obj);
+
+// 'fe'
+myArray.forEach(function(elem) {
+    // body...
+});
+
+// 'fi'
+for (prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+        // body...
+    }
+}
+
+// 'afn'
+function(arguments) {
+    // body...
+}
+
+// 'apply'
+methodName.apply(context, [arguments])
+
+// 'call'
+methodName.call(context, arguments)
+
+// 'iife'
+(function(window, document) {
+    // body...
+})(window, document);
+
+// 'fn'
+function methodName (arguments) {
+    // body...
+}
+
+// 'ofn'
+functionName: function(arguments) {
+    // body...
+}
+
+// 'call'
+methodName.call(context, arguments)
+
+// 'pr'
+ClassName.prototype.methodName = function(arguments) {
+    // body...
+}
+
+// 'jscn'
+TODO jscn
+
+// 'jsc'
+TODO jsc
+
+// 'jp'
+JSON.parse(obj);
+
+// 'js'
+JSON.stringify(obj);
+
+// 'de'
+debugger;
+
+// 'us'
+'use strict';
+
+// 'asd'
+assert.deepEqual(actual, expected);
+
+// 'ase'
+assert.equal(actual, expected);
+
+// 'asn'
+assert.notEqual(actual, expected);
+
+// 'me'
+module.exports = name;
+
+// 're'
+require('module');
+
+// 'rcp'
+import React from 'react'
+
+export default () => (
+  <div>Pure component!!</div>
+)
+
+// 'rcs'
+import React from 'react'
+
+export default class extends React.Component {
+    render () {
+        return (
+          <div>Component content</div>
+        )
+    }
+}
+
+// 'tmj'
+describe('UnitOfWork', () => {
+    describe('StateUnderTest', () => {
+        it('ExpectedBehaviour', () => {
+            // arrange
+
+            // act
+
+            // assert
+        })
+    })
+})
+
+// 'tmt'
+test('UnitOfWork_StateUnderTest_ExpectedBehaviour', t => {
+    // arrange
+
+    // act
+
+    // assert
+    t.end()
+})
+
+// 'si'
+setInterval(function() {
+    // body...
+}, delay);
+
+// 'st'
+setTimeout(function() {
+    // body...
+}, delay);
+
+
+
+
+
+
+//
